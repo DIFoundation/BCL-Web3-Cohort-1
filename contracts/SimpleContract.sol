8ffec623e9a53556329a514ef3f553ee38bf097b
DIFoundation <adeniranibrahim165@gmail.com>
2026-05-15 16:55:21 +0100

    Initial contract creation

---
+++ contracts/SimpleContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract SimpleContract {
+// What is Remix?, Setting Up Remix, Writing Your First Solidity Smart Contract 
+// Solidity Basics: Data Types and Variables, Functions, Storage and Memory, Contract Structure.
+    
+}
+
